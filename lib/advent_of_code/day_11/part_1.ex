95b8ff3965666091b565b31f8619285018ac146a
dviramontes <dviramontes@gmail.com>
2021-09-30 09:03:18 -0600

    wip day 11

--- lib/advent_of_code/day_11/part_1.ex
+++ lib/advent_of_code/day_11/part_1.ex
@@ -1,28 +1,59 @@
 defmodule AdventOfCode.Day11.Part1 do
-  def run(file_path) do
+  def run(file_path), do: run(file_path, 98, 95)
+
+  def run(file_path, rows, cols) do
     file_path
-    |> parse_input()
-    |> find_jolt_diff()
+    |> parse_input(rows, cols)
   end
 
-  def find_jolt_diff(jolts) do
-    {_position, ones, threes} =
-      jolts
-      |> Enum.reduce({0, 0, 0}, fn val, {current, ones, threes} ->
-        case val - current do
-          1 -> {val, ones + 1, threes}
-          3 -> {val, ones, threes + 1}
-        end
-      end)
+  def parse_input(file_path, rows, cols) do
+    grid =
+      file_path
+      |> File.stream!()
+      |> Stream.map(&String.trim/1)
+      |> Enum.map(&String.graphemes/1)
 
-    ones * (threes + 1)
+    for row <- 0..(rows - 1), col <- 0..(cols - 1), into: %{} do
+      {{row, col}, Enum.at(grid, row) |> Enum.at(col)}
+    end
   end
 
-  def parse_input(file_path) do
-    file_path
-    |> File.stream!()
-    |> Stream.map(&String.trim/1)
-    |> Stream.map(&String.to_integer/1)
-    |> Enum.sort()
+  def tick_until_stable(grid, cols, rows) do
+    case tick(grid, rows, cols) do
+      state when state == grid -> state
+      new_state -> tick_until_stable(new_state, cols, rows)
+    end
+  end
+
+  def tick(grid, rows, cols) do
+    for row <- 0..(rows - 1), col <- 0..(cols - 1), into: %{} do
+      current_state = Map.get(grid, {row, col})
+      {{row, col}, next_state({row, col}, current_state, grid)}
+    end
+  end
+
+  def next_state({col, row} = cursor, "L", grid) do
+    require IEx
+    adjacent_seats(cursor)
+    IEx.pry()
+  end
+
+  def next_state({col, row}, "#", grid) do
+  end
+
+  def next_state(_, current_state, _), do: current_state
+
+  def occupied?(seat), do: seat == "#"
+
+  def adjacent_seats({col, row}) do
+    [
+      {col - 1, row - 1},
+      {col, row - 1},
+      {col + 1, row - 1},
+      {col - 1, row},
+      {col + 1, row + 1},
+      {col, row + 1},
+      {col + 1, row + 1}
+    ]
   end
 end
