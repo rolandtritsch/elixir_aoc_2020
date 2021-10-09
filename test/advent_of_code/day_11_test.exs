587c09c15a320c63ae42003191bb83daa37f9389
Roland Tritsch <roland@tritsch.org>
2021-10-09 17:40:33 +0100

    Day11: Review. Adding another test. (#3)
    * Day11: Review. Adding another test.
    
    * Eleminate the hard-coding of the dimensions.

--- test/advent_of_code/day_11_test.exs
+++ test/advent_of_code/day_11_test.exs
@@ -1,10 +1,11 @@
 defmodule AdventOfCodeTest.Day11Test do
   use ExUnit.Case
 
-  describe "day 10" do
+  describe "day 11" do
     test "part 1, find occupied seats" do
-      assert AdventOfCode.Day11.Part1.run("./input/day_11_test.txt", 10, 10) == 37
+      assert AdventOfCode.Day11.Part1.run("./input/day_11_test.txt") == 37
       assert AdventOfCode.Day11.Part1.run("./input/day_11.txt") == 2412
+      assert AdventOfCode.Day11.Part1.run("./input/day_11b.txt") == 2289
     end
 
     test "part 2" do
