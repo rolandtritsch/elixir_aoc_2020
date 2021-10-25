101eac51b48c54147bdf0b881cde3a35e444b0d8
Roland Tritsch <roland@tritsch.org>
2021-10-25 16:30:39 +0100

    Day14: More datastructs, docs, specs, tests for readability.

--- lib/advent_of_code/day_14/part_1.ex
+++ lib/advent_of_code/day_14/part_1.ex
@@ -1,25 +1,125 @@
 defmodule AdventOfCode.Day14.Part1 do
+  @moduledoc """
+  Solving Part 1 of Day 14.
+
+  Reading the input file into a list of instructions.
+  Then I am evaluating every instruction one-by-one.
+  Every instruction will update the current mask or will
+  update a memory address (while applying the mask).
+  When we are done with all instructions, we just
+  sum up all values that are in memory.
+  """
+
+  # data structures
+
+  defmodule Instruction.Mask do
+    @typedoc """
+    And instruction of type mask.
+    """
+    defstruct mask: nil
+
+    @type t(mask) :: %Instruction.Mask{mask: mask}
+    @type t :: %Instruction.Mask{mask: list(String.t())}        
+  end
+
+  defmodule Instruction.Mem do
+    @typedoc """
+    And instruction of type mem.
+    """
+    defstruct addr: nil, value: nil 
+
+    @type t(addr, value) :: %Instruction.Mem{addr: addr, value: value}
+    @type t :: %Instruction.Mem{addr: integer, value: integer}        
+  end
+                             
+  defmodule Memory do
+    @typedoc """
+    A block of memory. And the current mask.
+    """
+    defstruct memory: %{}, mask: []
+
+    @type t(memory, mask) :: %Memory{memory: memory, mask: mask}
+    @type t :: %Memory{memory: %{integer => integer}, mask: list(String.t())}
+  end
+
+  # public functions
+
+  @doc """
+  Takes a file_path and solves the problem. Returns the solution.
+  """
+  @spec run(String.t()) :: integer
+  def run(file_path)
+  
   def run(file_path) do
+    instructions = file_path |> parse_input()
+    final_memory = instructions |> Enum.reduce(%Memory{}, &evaluate/2)
+    final_memory.memory |> Map.values() |> Enum.sum()
+  end
+
+  # private functions
+
+  @doc """
+  Takes a file_path. Returns the instructions to process.
+  """
+  @spec parse_input(String.t()) :: list(Instruction.Mask.t() | Instruction.Mem.t())
+  def parse_input(file_path)
+  
+  def parse_input(file_path) do
     file_path
-    |> parse_input()
-    |> evaluate()
-    |> elem(0)
-    |> Map.values()
-    |> Enum.sum()
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Enum.map(&parse_line/1)
   end
 
-  def evaluate(instructions) do
-    Enum.reduce(instructions, {%{}, ""}, &evaluate_instruction/2)
+  @doc """
+  Parse a line. Return an instruction.
+  """
+  @spec parse_line(String.t()) :: Instruction.Mask.t() | Instruction.Mem.t()
+  def parse_line(line)
+  
+  def parse_line("mask = " <> mask_str) do
+    mask =
+      mask_str
+      |> String.graphemes()
+      |> Enum.reverse()
+
+    %Instruction.Mask{mask: mask}
   end
 
-  def evaluate_instruction({:mask, new_mask}, {memory, _mask}) do
-    {memory, new_mask}
+  def parse_line("mem[" <> memory_str) do
+    {addr, rest} = Integer.parse(memory_str)
+    "] = " <> value_str = rest
+
+    %Instruction.Mem{addr: addr, value: String.to_integer(value_str)}
   end
 
-  def evaluate_instruction({:mem, addr, value}, {memory, mask}) do
-    {Map.put(memory, addr, apply_mask(mask, value)), mask}
+  @doc """
+  Evaluate an instruction. Return the updated memory. 
+  """
+  @spec evaluate(Instruction.Mask.t() | Instruction.Mem.t(), Memory.t()) :: Memory.t() 
+  def evaluate(instruction, memory)
+  
+  def evaluate(
+    %Instruction.Mask{} = mask,
+    %Memory{} = memory
+  ) do
+    %{memory | mask: mask.mask}
   end
 
+  def evaluate(
+    %Instruction.Mem{} = instr_mem,
+    %Memory{} = memory
+  ) do
+    new_value = memory.mask |> apply_mask(instr_mem.value)
+    %{memory | memory: Map.put(memory.memory, instr_mem.addr, new_value)}
+  end
+
+  @doc """
+  Apply the given mask to the given value. Return the resulting value.
+  """
+  @spec apply_mask(String.t(), integer) :: integer
+  def apply_mask(mask, value)
+  
   def apply_mask(mask, value) do
     binary = Integer.digits(value, 2) |> Enum.reverse()
 
@@ -32,27 +132,5 @@ defmodule AdventOfCode.Day14.Part1 do
     end
     |> Enum.reverse()
     |> Integer.undigits(2)
-  end
-
-  def parse_input(file_path) do
-    file_path
-    |> File.stream!()
-    |> Stream.map(&String.trim/1)
-    |> Enum.map(&parse_line/1)
-  end
-
-  def parse_line("mask = " <> mask_str) do
-    mask =
-      mask_str
-      |> String.graphemes()
-      |> Enum.reverse()
-
-    {:mask, mask}
-  end
-
-  def parse_line("mem[" <> memory_str) do
-    {addr, rest} = Integer.parse(memory_str)
-    "] = " <> value_str = rest
-    {:mem, addr, String.to_integer(value_str)}
   end
 end
--- test/advent_of_code/day_14_test.exs
+++ test/advent_of_code/day_14_test.exs
@@ -1,10 +1,63 @@
 defmodule AdventOfCodeTest.Day14Test do
   use ExUnit.Case
 
+  alias AdventOfCode.Day14.Part1
+
+  setup_all do
+    file_path = "./input/day_14_test.txt"
+    input = file_path |> Part1.parse_input()
+
+    {:ok, input: input}
+  end
+
   describe "day 14" do
+    test "parse_input", state do
+      expected = [
+        %Part1.Instruction.Mask{
+          mask: [
+            "X", "0", "X", "X", "X", "X", "1", "X", "X", "X", "X",
+            "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",
+            "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",
+            "X", "X", "X"
+          ]
+        },
+        %Part1.Instruction.Mem{addr: 8, value: 11},
+        %Part1.Instruction.Mem{addr: 7, value: 101},
+        %Part1.Instruction.Mem{addr: 8, value: 0}
+      ]
+
+      assert state[:input] == expected
+    end
+
+    test "apply_mask", state do
+      mask_instr = state[:input] |> Enum.at(0)
+      mem_instr = state[:input] |> Enum.at(1)
+      
+      assert Part1.apply_mask(mask_instr.mask, mem_instr.value) == 73
+    end
+
+    test "evaluate - mask", state do
+      mask_instr = state[:input] |> Enum.at(0)
+      
+      expected = %Part1.Memory{mask: mask_instr.mask}
+
+      assert Part1.evaluate(mask_instr, %Part1.Memory{}) == expected
+    end
+
+    test "evaluate - mem", state do
+      mask_instr = state[:input] |> Enum.at(0)
+      mem_instr = state[:input] |> Enum.at(1)
+
+      expected = %Part1.Memory{mask: mask_instr.mask, memory: %{8 => 73}}
+
+      memory = Part1.evaluate(mask_instr, %Part1.Memory{})
+      assert Part1.evaluate(mem_instr, memory) == expected
+    end
+    
     test "part 1" do
-      assert AdventOfCode.Day14.Part1.run("./input/day_14_test.txt") == 165
-      assert AdventOfCode.Day14.Part1.run("./input/day_14.txt") == 4886706177792
+      assert Part1.run("./input/day_14_test.txt") == 165
+      assert Part1.run("./input/day_14.txt") == 4886706177792
+      assert Part1.run("./input/day_14b.txt") == 7440382076205
     end
 
     test "part 2" do
